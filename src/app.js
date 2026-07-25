a4d88832a697d0b95f5d12c31480f95352413cbe
Kalpesh Patil <kalpeshpatil65555@gmail.com>
2026-07-25 19:01:03 +0530

    Get Req Call

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express"
+
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send("Dashboard");
+})
+
+app.get("/home",(req,res)=>{
+    res.send("Home");
+})
+
+app.get("/about",(req,res)=>{
+    res.send("About");
+})
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000...");
+})
