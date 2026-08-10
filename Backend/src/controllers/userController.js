239145cce10cefa4d83a932833440a353f5c108e
Kalpesh Patil <kalpeshpatil65555@gmail.com>
2026-08-10 14:07:48 +0530

    dfndk

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -64,7 +64,7 @@ export const userFeed = async (req, res) => {
 
     // Pagination
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+    const limit = parseInt(req.query.limit) || 100;
     const skip = (page - 1) * limit;
 
     // Find all connection requests of logged-in user
